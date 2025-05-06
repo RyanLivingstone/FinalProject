f81faf61a27708f47b587f783e0ccbb7212f3e48
RyanLivingstone <ryanlive02@gmail.com>
2025-05-06 13:05:41 -0400

    Added enemy's detection range for the player

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -4,6 +4,7 @@ public class EnemyMovement : MonoBehaviour
 {
     private Transform target;
     public float speed;
+    public float detectionRange = 10f;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -15,6 +16,11 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+    // Finds distance between the enemy and player and if that distance is shorter than the detectionRange it will follow through with the enemy's movement
+        float distanceFromPlayer =  Vector2.Distance(transform.position, target.position);
+
+        if (distanceFromPlayer <= detectionRange){
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
     }
 }
