b669c2bae5f180f2dc4c087abdbc580686621751
natellerman <natellerman@gmail.com>
2025-05-05 20:31:31 -0400

    Updated health script

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -3,14 +3,33 @@ using UnityEngine.SceneManagement;
 
 public class Health : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
-{
-    if (other.CompareTag("Player"))
-    {
-        Die();
-    }
-}
+    public int maxHealth = 10;
+    public int currentHealth;
 
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Skull"))
+        {
+            Die();
+        }
+
+        else if (other.CompareTag("Food"))
+        {
+            RestoreHealth(2);
+            Destroy(other.gameObject);
+        }
+    }
+
+    public void RestoreHealth(int amount)
+    {
+        currentHealth = Mathf.Min(currentHealth + amount, maxHealth);
+        Debug.Log("Health restored. Current health: " + currentHealth);
+    }
 
     private void Die()
     {
