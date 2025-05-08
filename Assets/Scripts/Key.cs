fefbabe69d9cbdbbd5ad56f218158991293d6f15
natellerman <natellerman@gmail.com>
2025-05-07 20:50:31 -0400

    Changed all scripts to be 2D

--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -2,9 +2,8 @@ using UnityEngine;
 
 public class Key : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-
         if (other.CompareTag("Player"))
         {
             PlayerInventory playerInventory = other.GetComponent<PlayerInventory>();
