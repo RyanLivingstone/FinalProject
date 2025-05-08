6bb620796c3c79b68e4ca8ae5262924b685db8f0
RyanLivingstone <ryanlive02@gmail.com>
2025-05-08 10:04:26 -0400

    Fixed button and made sure they worked to switch scenes

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,8 +3,9 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
+    public string IntroScene = "";
     public void PlayGame()
     {
-        SceneManager.LoadScene("ItemsScene");
+        SceneManager.LoadScene(IntroScene);
     }
 }
